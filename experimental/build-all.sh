986792d8a5cb15885d54ea5d9e4d4fc66491e261
Trevor Dolby <trevor.dolby@ibm.com>
2024-11-06 19:20:04 +0000

    Modify mqclient builds
    Signed-off-by: Trevor Dolby <trevor.dolby@ibm.com>

--- experimental/build-all.sh
+++ experimental/build-all.sh
@@ -6,6 +6,8 @@ export DOWNLOAD_URL=https://public.dhe.ibm.com/ibmdl/export/pub/software/websphe
 #export DOWNLOAD_URL=http://192.168.1.70:49177/ace/13.0.1.0-ACE-LINUX64-EVALUATION.tar.gz
 export PRODUCT_VERSION=13.0.1.0
 
+# Used for ace-minimal and ace-devcontainer mqclient variants
+export MQ_DOWNLOAD_URL=https://public.dhe.ibm.com/ibmdl/export/pub/software/websphere/messaging/mqdev/redist/9.4.1.0-IBM-MQC-Redist-LinuxX64.tar.gz
 #export DOWNLOAD_CONNECTION_COUNT=10
 
 
@@ -16,11 +18,9 @@ cd ace-minimal
 docker build --build-arg DOWNLOAD_URL --build-arg DOWNLOAD_CONNECTION_COUNT -t ace-minimal:${PRODUCT_VERSION}-alpine -f Dockerfile.alpine .
 docker build --build-arg DOWNLOAD_URL --build-arg DOWNLOAD_CONNECTION_COUNT -t ace-minimal:${PRODUCT_VERSION}-alpine-java8 -f Dockerfile.alpine-java8 .
 docker build --build-arg DOWNLOAD_URL --build-arg DOWNLOAD_CONNECTION_COUNT -t ace-minimal:${PRODUCT_VERSION}-ubuntu -f Dockerfile.ubuntu .
+docker build --build-arg DOWNLOAD_URL --build-arg DOWNLOAD_CONNECTION_COUNT --build-arg MQ_DOWNLOAD_URL -t ace-minimal:${PRODUCT_VERSION}-alpine-mqclient -f Dockerfile.alpine .
 
-cd mqclient
-docker build --build-arg BASE_IMAGE=ace-minimal:${PRODUCT_VERSION}-alpine -t ace-minimal:${PRODUCT_VERSION}-alpine-mqclient -f Dockerfile.mqclient .
-
-cd ../../ace-full
+cd ../ace-full
 docker build --build-arg DOWNLOAD_URL --build-arg DOWNLOAD_CONNECTION_COUNT -t ace-full:${PRODUCT_VERSION}-ubuntu -f Dockerfile.ubuntu .
 
 cd ../ace-basic
@@ -29,7 +29,7 @@ docker build --build-arg DOWNLOAD_URL --build-arg DOWNLOAD_CONNECTION_COUNT -t a
 cd ../devcontainers
 docker build --build-arg DOWNLOAD_URL --build-arg DOWNLOAD_CONNECTION_COUNT -t ace-devcontainer:${PRODUCT_VERSION} -f Dockerfile .
 docker build --build-arg DOWNLOAD_URL --build-arg DOWNLOAD_CONNECTION_COUNT -t ace-minimal-devcontainer:${PRODUCT_VERSION} -f Dockerfile.ace-minimal .
-docker build --build-arg DOWNLOAD_URL --build-arg DOWNLOAD_CONNECTION_COUNT -t ace-devcontainer-mqclient:${PRODUCT_VERSION} -f Dockerfile.mqclient .
+docker build --build-arg DOWNLOAD_URL --build-arg DOWNLOAD_CONNECTION_COUNT --build-arg MQ_DOWNLOAD_URL -t ace-devcontainer-mqclient:${PRODUCT_VERSION} -f Dockerfile.mqclient .
 
 cd ../sample
 # Normally only one of these would be built, and would be tagged with an application version
