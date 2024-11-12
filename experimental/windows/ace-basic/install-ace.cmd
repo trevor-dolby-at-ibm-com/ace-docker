eebade41ee5249873493d5d44ef0c5e974cb24cf
Trevor Dolby <trevor.dolby@ibm.com>
2024-11-12 16:50:05 -0600

    Cleanup
    Signed-off-by: Trevor Dolby <trevor.dolby@ibm.com>

--- experimental/windows/ace-basic/install-ace.cmd
+++ experimental/windows/ace-basic/install-ace.cmd
@@ -4,7 +4,9 @@ dir c:\tmp
 dir c:\tmp\ace-unzip
 
 cd c:\tmp\ace-unzip\
-.\ACESetup13.0.1.0.exe /quiet LICENSE_ACCEPTED=true InstallFolder=C:\ace-v13 InstallToolkit=0 InstallGAC=0
+REM .\ACESetup13.0.1.0.exe /quiet LICENSE_ACCEPTED=true InstallFolder=C:\ace-v13 InstallToolkit=0 InstallGAC=0
+.\ACESetup13.0.1.0.exe -silent -installFolder C:\ace-13 -licenseAccept yes -anonymousUsageStatistics no -installToolkit no -installWSRRnodes no -installElectronApp no 
+
 REM Install the .Net support DLLs
 call c:\ace-v13\server\bin\runCommand.cmd C:\ace-v13\server\bin\mqsiAssemblyInstall -i C:\ace-v13\server\bin\IBM.Broker.Plugin.dll
 call c:\ace-v13\server\bin\runCommand.cmd C:\ace-v13\server\bin\mqsiAssemblyInstall -i C:\ace-v13\server\bin\IBM.Broker.Support.dll
